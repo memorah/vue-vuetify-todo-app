015fb398d7ee0b8e44416008d7f5b532ccb268b2
memorah <filip.mackov@gmail.com>
2022-10-05 11:09:23 +0200

    delete btn add

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,15 +1,26 @@
 <template>
   <v-list class="pt-0" flat>
     <div v-for="task in tasks" :key="task.id">
-      <v-list-item>
-        <template v-slot:default="{ active }">
+      <v-list-item
+        @click="doneTask(task.id)"
+        :class="{ 'green lighten-5': task.done }"
+      >
+        <template v-slot:default>
           <v-list-item-action>
-            <v-checkbox :input-value="active"></v-checkbox>
+            <v-checkbox :input-value="task.done"></v-checkbox>
           </v-list-item-action>
 
           <v-list-item-content>
-            <v-list-item-title>{{ task.title }}</v-list-item-title>
+            <v-list-item-title
+              :class="{ 'text-decoration-line-through': task.done }"
+              >{{ task.title }}</v-list-item-title
+            >
           </v-list-item-content>
+          <v-list-item-action>
+            <v-btn @click.stop="deleteTask(task.id)" icon>
+              <v-icon color="primary lighten-1">mdi-delete-forever</v-icon>
+            </v-btn>
+          </v-list-item-action>
         </template>
       </v-list-item>
       <v-divider></v-divider>
@@ -26,17 +37,29 @@ export default {
         {
           id: 1,
           title: "Wake Up",
+          done: false,
         },
         {
           id: 2,
           title: "Get bananas",
+          done: false,
         },
         {
-          id: 2,
+          id: 3,
           title: "Eat bananas",
+          done: false,
         },
       ],
     };
+  },
+  methods: {
+    doneTask(id) {
+      let task = this.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
   },
 };
 </script>
