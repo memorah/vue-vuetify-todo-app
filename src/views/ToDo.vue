dd3e69ca3792c0db1a185ad78ed277a335eba466
memorah <filip.mackov@gmail.com>
2022-10-05 10:33:27 +0200

    edit v-for fix

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,6 +1,6 @@
 <template>
   <v-list flat>
-    <v-list-item v-for="item in tasksItems" :key="item.id">
+    <v-list-item v-for="task in tasks" :key="task.id">
       <template v-slot:default="{ active }">
         <v-list-item-action>
           <v-checkbox :input-value="active"></v-checkbox>
@@ -18,20 +18,22 @@
 export default {
   name: "ToDo",
   data() {
-    tasksItems: [
-      {
-        id: 1,
-        title: "Wake Up",
-      },
-      {
-        id: 2,
-        title: "2",
-      },
-      {
-        id: 2,
-        title: "3",
-      },
-    ];
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Wake Up",
+        },
+        {
+          id: 2,
+          title: "Get bananas",
+        },
+        {
+          id: 2,
+          title: "Eat bananas",
+        },
+      ],
+    };
   },
 };
 </script>
