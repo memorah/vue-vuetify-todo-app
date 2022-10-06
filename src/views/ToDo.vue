4f1e1724505915a5ec01ec44f346f113bfec273b
memorah <filip.mackov@gmail.com>
2022-10-06 09:04:04 +0200

    edit

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,68 +1,77 @@
 <template>
-  <v-list class="pt-0" flat>
-    <div>
-      <v-text-field
-        v-model="newTaskTitle"
-        @click:append="addTask"
-        @keyop.enter="addTask"
-        class="pa-3"
-        outlined
-        label="Add Task"
-        append-icon="mdi-plus"
-        hide-details
-        clearable
-      ></v-text-field>
-      <v-list-item
-        v-for="task in tasks"
-        :key="task.id"
-        @click="doneTask(task.id)"
-        :class="{ 'green lighten-5': task.done }"
-      >
-        <template v-slot:default>
-          <v-list-item-action>
-            <v-checkbox :input-value="task.done"></v-checkbox>
-          </v-list-item-action>
+  <div class="home">
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
+    ></v-text-field>
 
-          <v-list-item-content>
-            <v-list-item-title
-              :class="{ 'text-decoration-line-through': task.done }"
-              >{{ task.title }}</v-list-item-title
-            >
-          </v-list-item-content>
-          <v-list-item-action>
-            <v-btn @click.stop="deleteTask(task.id)" icon>
-              <v-icon color="primary lighten-1">mdi-delete-forever</v-icon>
-            </v-btn>
-          </v-list-item-action>
-        </template>
-      </v-list-item>
-      <v-divider></v-divider>
+    <v-list v-if="tasks.length" class="pt-0" flat>
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5': task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+              >
+                {{ task.title }}
+              </v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn @click.stop="deleteTask(task.id)" icon>
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+    <div v-else class="no-tasks-box">
+      <div class="no-tasks">
+        <v-icon size="50" color="primary"> mdi-meditation </v-icon>
+        <div class="text-h5 primary--text">No tasks</div>
+      </div>
     </div>
-  </v-list>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ToDo",
+  name: "ToDO",
   data() {
     return {
       newTaskTitle: "",
       tasks: [
-        {
-          id: 1,
-          title: "Wake Up",
-          done: false,
-        },
-        {
-          id: 2,
-          title: "Get bananas",
-          done: false,
-        },
-        {
-          id: 3,
-          title: "Eat bananas",
-          done: false,
-        },
+        // {
+        //   id: 1,
+        //   title: 'Wake up',
+        //   done: false
+        // },
+        // {
+        //   id: 2,
+        //   title: 'Get bananas',
+        //   done: false
+        // },
+        // {
+        //   id: 3,
+        //   title: 'Eat bananas',
+        //   done: false
+        // }
       ],
     };
   },
@@ -74,8 +83,8 @@ export default {
         done: false,
       };
       this.tasks.push(newTask);
+      this.newTaskTitle = "";
     },
-
     doneTask(id) {
       let task = this.tasks.filter((task) => task.id === id)[0];
       task.done = !task.done;
@@ -86,3 +95,17 @@ export default {
   },
 };
 </script>
+
+<style lang="sass">
+.no-tasks-box
+  position: absolute
+  top: 50%
+  left: 50%
+  transform: translate( -50%, -50%)
+.no-tasks
+  display: flex
+  flex-direction: column
+
+  align-items: center
+  opacity: 0.5
+</style>
