3ad85fd4f1533ad2d91bf59326d8d52635189f67
memorah <filip.mackov@gmail.com>
2022-10-06 09:53:47 +0200

    vuex add task

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,36 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    tasks: [
+      {
+        id: 1,
+        title: "Wake up",
+        done: false,
+      },
+      {
+        id: 2,
+        title: "Get bananas",
+        done: false,
+      },
+      {
+        id: 3,
+        title: "Eat bananas",
+        done: false,
+      },
+    ],
+  },
   getters: {},
-  mutations: {},
+  mutations: {
+    addTask(state, newTaskTitle) {
+      let newTask = {
+        id: Date.now(),
+        title: newTaskTitle,
+        done: false,
+      };
+      state.tasks.push(newTask);
+    },
+  },
   actions: {},
   modules: {},
 });
--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -12,8 +12,8 @@
       clearable
     ></v-text-field>
 
-    <v-list v-if="tasks.length" class="pt-0" flat>
-      <div v-for="task in tasks" :key="task.id">
+    <v-list v-if="$store.state.tasks.length" class="pt-0" flat>
+      <div v-for="task in $store.state.tasks" :key="task.id">
         <v-list-item
           @click="doneTask(task.id)"
           :class="{ 'blue lighten-5': task.done }"
@@ -56,41 +56,12 @@ export default {
   data() {
     return {
       newTaskTitle: "",
-      tasks: [
-        // {
-        //   id: 1,
-        //   title: 'Wake up',
-        //   done: false
-        // },
-        // {
-        //   id: 2,
-        //   title: 'Get bananas',
-        //   done: false
-        // },
-        // {
-        //   id: 3,
-        //   title: 'Eat bananas',
-        //   done: false
-        // }
-      ],
     };
   },
   methods: {
     addTask() {
-      let newTask = {
-        id: Date.now(),
-        title: this.newTaskTitle,
-        done: false,
-      };
-      this.tasks.push(newTask);
+      this.$store.commit("addTask", this.newTaskTitle);
       this.newTaskTitle = "";
-    },
-    doneTask(id) {
-      let task = this.tasks.filter((task) => task.id === id)[0];
-      task.done = !task.done;
-    },
-    deleteTask(id) {
-      this.tasks = this.tasks.filter((task) => task.id !== id);
     },
   },
 };
