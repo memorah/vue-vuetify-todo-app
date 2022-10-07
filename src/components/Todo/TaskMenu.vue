6cf86b8a9f7d27168ad0e46b02ccd5d074f0bbbb
memorah <filip.mackov@gmail.com>
2022-10-07 10:16:38 +0200

    task menu

---
+++ src/components/Todo/TaskMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-menu bottom left>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="primary" icon v-bind="attrs" v-on="on">
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </template>
+
+    <v-list>
+      <v-list-item v-for="(item, i) in items" :key="i">
+        <v-list-item-icon>
+          <v-icon v-text="item.icon"></v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      { title: "Edit", icon: "mdi-pencil" },
+      { title: "Due Date", icon: "mdi-alarm-check" },
+      { title: "Delete", icon: "mdi-delete" },
+    ],
+  }),
+};
+</script>
+
+<style></style>
