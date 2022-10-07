a96e8cca1c2ca7d887b0136ff5f57ef108eb7b80
memorah <filip.mackov@gmail.com>
2022-10-07 15:12:29 +0200

    set searchTask only close if emty

--- src/components/Todo/Tools/SearchTask.vue
+++ src/components/Todo/Tools/SearchTask.vue
@@ -1,9 +1,39 @@
 <template>
-  <v-text-field label="Filled" filled></v-text-field>
+  <v-text-field
+    v-model="search"
+    @focus="searchClosed = false"
+    @blur="searchClosed = true"
+    class="expanding-search mt-1"
+    :class="{ closed: searchClosed && !search }"
+    placeholder="Search"
+    prepend-inner-icon="mdi-magnify"
+    filled
+    dense
+    clearable
+  ></v-text-field>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      search: null,
+      searchClosed: true,
+    };
+  },
+};
 </script>
 
-<style></style>
+<style lang="sass">
+.expanding-search
+    transition: max-width .5s
+
+.v-input__slot
+  cursor: pointer !important
+  &:before, &:after
+    border-color: transparent !important
+.closed
+    max-width: 45px
+    .v-input__slot
+        background: transparent !important
+</style>
